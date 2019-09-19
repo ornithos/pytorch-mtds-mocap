0d99d59cb22c6c4ab1b88bc2c6f261ac75ebd4be
Alex Bird <abird@turing.ac.uk>
2019-09-19 08:55:06 +0100

    change model save path for different train set size

--- src/parseopts4bmark.py
+++ src/parseopts4bmark.py
@@ -126,4 +126,6 @@ def get_model_save_dir(args):
                   'depth_{0}'.format(args.num_layers),
                   'size_{0}'.format(args.size),
                   'lr_{0}'.format(args.learning_rate),
-                  'residual_vel' if args.residual_velocities else 'not_residual_vel'))
+                  'residual_vel' if args.residual_velocities else 'not_residual_vel',
+                  'train_{0}'.format('all' if args.train_set_size == -1 else args.train_set_size))
+                  )
